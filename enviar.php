23083bffb27626581fb785b0ce5dccade9bd4941
Gilnei Laurindo <107190484+gilneilaurindo@users.noreply.github.com>
2024-07-04 00:28:18 -0300

    PHP
    enviar

---
+++ enviar.php
@@ -0,0 +1,45 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $assunto = $_POST['assunto'];
+    $mensagem = $_POST['mensagem'];
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // Configurações do servidor
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Usando o servidor SMTP do Gmail
+        $mail->SMTPAuth = true;
+        $mail->Username = 'gilneycriative@gmail.com'; // Seu endereço de e-mail
+        $mail->Password = 'sua-senha'; // Sua senha de aplicativo
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        // Destinatário
+        $mail->setFrom($email, $nome);
+        $mail->addAddress('gilneycriative@gmail.com'); // Seu e-mail para receber as mensagens
+
+        // Conteúdo
+        $mail->isHTML(true);
+        $mail->Subject = $assunto;
+        $mail->Body = "Nome: $nome<br>Email: $email<br>Telefone: $telefone<br><br>Mensagem:<br>$mensagem";
+
+        $mail->send();
+        echo 'Mensagem enviada com sucesso!';
+    } catch (Exception $e) {
+        echo "A mensagem não pode ser enviada. Erro do Mailer: {$mail->ErrorInfo}";
+    }
+} else {
+    echo 'Método de requisição inválido.';
+}
+?>
